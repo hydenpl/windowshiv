bdeadf4595343c254531b157b87a9490f30d912a
unknown <hyden@op.pl>
2014-07-25 23:06:29 +0200

    ch3 z ksiazki

--- js/index.js
+++ js/index.js
@@ -1,40 +1,60 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var canvas;
+var ctx;
 
-function init(){
-    var canvas = initFullScreenCanvas("mainCanvas");
-    
+function init() {
+    canvas = initFullScreenCanvas("mainCanvas");
+    ctx = canvas.getContext('2d');
+    repaint();
+    reorient();
 }
 
-function initFullScreenCanvas(canvasId){
+function initFullScreenCanvas(canvasId) {
     var canvas = document.getElementById(canvasId);
     resizeCanvas(canvas);
-    windows.addEventListener("resize", function(){
+    window.addEventListener("resize", function() {
         resizeCanvas(canvas);
     });
     return canvas;
 }
 
-function resizeCanvas(canvas){
+function resizeCanvas(canvas) {
     canvas.width = document.width || document.body.clientWidth;
     canvas.height = document.height || document.body.clientHeight;
+    repaint();
 }
+
+function repaint() {
+    if (!ctx)
+        return;
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    reorient();
+    ctx.fillStyle = "darkgreen";
+    ctx.fillRect(10, 10, 250, 30);
+    ctx.fillRect(120, 10, 30, 300);
+}
+
+function reorient() {
+    var angle = window.orientation;
+    if (angle) {
+        ctx.fillStyle = "red";
+        ctx.font="20px Georgia";
+        ctx.fillText(String(angle),50,50);
+        
+        var rot = -Math.PI * (angle / 180);
+//        ctx.rotate(rot);
+        ctx.translate(angle === -90 ? canvas.width : 0,
+                angle === 90 ? canvas.height : 0);
+        ctx.rotate(rot);
+    }
+}
+
+
+
+
+
 
 
 
@@ -45,7 +65,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        
+
     },
     // Bind Event Listeners
     //
