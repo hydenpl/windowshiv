1a8b14f5b1c2d38cac5f1e86c53e7f1ec2f3c617
hyden <bartoszukpiotr@gmail.com>
2015-09-21 00:36:04 +0200

    zmiana nazwy

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ function initGame(key) {
     
     $('#content').load('game.html?v='+Math.random(), function() {
         $('.header').show();
-        $('h2.header-title').text("(i)grasz?");
+//        $('h2.header-title').text("(i)grasz?");
         $(".back-button").addClass("link").data("link", "main")
 //        drawInfo('back-canvas');
 //        $(".back-button").addClass("link").data("link", "instructions")
